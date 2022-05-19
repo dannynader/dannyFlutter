bdc7838c344b4cbbe2bfd879d680b4e9b930d155
FlutLab.io <>
2022-05-19 11:20:53 +0000

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyApp()); //danny
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
